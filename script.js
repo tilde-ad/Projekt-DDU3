410243f0491a44f0458337e5655f856ba469e324
JosefinBack <josefin.m.mansson@gmail.com>
2025-05-14 12:11:08 +0200

    Flyttade kod
    Flyttade klassen från test.js till index.js

---
+++ script.js
@@ -0,0 +1,30 @@
+class Dogbreed {
+    //static Breeds = ["huskies", "rasker", "Collie"]  //här måste vo göra en request för att få dogbreedsen
+    // men då måste vi kanske göra en static allbreeds funktion
+
+    static Breeds = []
+    static async fetchBreed() {
+        let response = await fetch("https://dog.ceo/api/breeds/list/all")
+        let data = await response.json()
+        for (let breed in data.message) {
+            Dogbreed.Breeds.push(breed)
+        }
+    }
+
+    get dogBreed() {
+        return this._dogBreed
+    }
+    set dogBreed(value) {
+        if (!Dogbreed.Breeds.includes(value)) {
+            console.log("error")
+        }
+        this._dogBreed = value
+    }
+}
+
+async function driver() {
+    await Dogbreed.fetchBreed()
+    console.log(Dogbreed.Breeds)
+}
+driver()
+
