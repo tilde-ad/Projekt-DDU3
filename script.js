21793c696de624cac32bc97a7e0df46968c45c49
Sara <so013872@gmail.com>
2025-05-14 14:12:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 class Dogbreed {
-    //static Breeds = ["huskies", "rasker", "Collie"]  //här måste vo göra en request för att få dogbreedsen
-    // men då måste vi kanske göra en static allbreeds funktion
-
-    static Breeds = []
-    static async fetchBreed() {
+    constructor (){
+        this.Breeds = []
+    }
+  
+    async fetchBreed (){
         let response = await fetch("https://dog.ceo/api/breeds/list/all")
         let data = await response.json()
-        for (let breed in data.message) {
-            Dogbreed.Breeds.push(breed)
+        for(let breed in data.message){
+            this.Breeds.push(breed)
         }
     }
-
-    get dogBreed() {
+  
+    get dogBreed(){ 
         return this._dogBreed
     }
-    set dogBreed(value) {
-        if (!Dogbreed.Breeds.includes(value)) {
+    set  dogBreed(value){
+        if(!this.Breeds.includes(value)){
             console.log("error")
         }
         this._dogBreed = value
     }
 }
 
-async function driver() {
-    await Dogbreed.fetchBreed()
-    console.log(Dogbreed.Breeds)
+async function driver () {
+    let dogManager = new Dogbreed()  //instans av en klass som hanterar hundraser
+    await dogManager.fetchBreed()
+    console.log(dogManager.Breeds)
 }
 driver()
 
