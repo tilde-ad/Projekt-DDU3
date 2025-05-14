afc1d71ff7d529ec6be198bb49f8c774e60e76e4
JosefinBack <josefin.m.mansson@gmail.com>
2025-05-14 12:09:53 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-testing-API
+testing - API
 async function getDogFact() {
     const response = await fetch("http://localhost:8000/dogfact");
     const data = await response.json();
@@ -7,32 +7,31 @@ async function getDogFact() {
 
 getDogFact();
 
-=======
 class Dogbreed {
     //static Breeds = ["huskies", "rasker", "Collie"]  //här måste vo göra en request för att få dogbreedsen
     // men då måste vi kanske göra en static allbreeds funktion
 
     static Breeds = []
-    static async fetchBreed (){
+    static async fetchBreed() {
         let response = await fetch("https://dog.ceo/api/breeds/list/all")
         let data = await response.json()
-        for(let breed in data.message){
+        for (let breed in data.message) {
             Dogbreed.Breeds.push(breed)
         }
     }
-  
-    get dogBreed(){ 
+
+    get dogBreed() {
         return this._dogBreed
     }
-    set  dogBreed(value){
-        if(!Dogbreed.Breeds.includes(value)){
+    set dogBreed(value) {
+        if (!Dogbreed.Breeds.includes(value)) {
             console.log("error")
         }
         this._dogBreed = value
     }
 }
 
-async function driver () {
+async function driver() {
     await Dogbreed.fetchBreed()
     console.log(Dogbreed.Breeds)
 }
