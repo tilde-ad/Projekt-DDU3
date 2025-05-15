8574be4f7560edd206911e92ef0b3c43e84b3d80
Sara <so013872@gmail.com>
2025-05-15 13:47:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 class Dogbreed {
-    constructor (){
-        this.Breeds = []
+    constructor({ name, description }) {
+        this.name = name;
+        this.description = description;
+        this.breeds = []
+        this.instances = []
     }
-  
-    async fetchBreed (){
-        let response = await fetch("https://dog.ceo/api/breeds/list/all")
-        let data = await response.json()
-        for(let breed in data.message){
-            this.Breeds.push(breed)
-        }
+
+    async fetchBreed() {
+        let response = await fetch("http://localhost:8000/dogbreed");
+        let data = await response.json();
+        this.breeds = data;
+        this.instances = data.map(breedObj => new Dogbreed(breedObj));
     }
-  
-    get dogBreed(){ 
+
+    get dogBreed() {
         return this._dogBreed
     }
-    set  dogBreed(value){
-        if(!this.Breeds.includes(value)){
-            console.log("error")
+    set dogBreed(value) {
+        if (!this.breeds.some(b => b.name === value)) {
+            console.log("error");
+            return;
         }
-        this._dogBreed = value
+        this._dogBreed = value;
     }
 }
 
-async function driver () {
-    let dogManager = new Dogbreed()  //instans av en klass som hanterar hundraser
-    await dogManager.fetchBreed()
-    console.log(dogManager.Breeds)
+async function driver() {
+    const dogBreedmanager = new Dogbreed({name: "", description: ""})
+    await dogBreedmanager.fetchBreed();
+    console.log(dogBreedmanager.instances);
 }
-driver()
 
+driver();
 
 //få bilder och blanda dem
 async function getDogPic() {
