1d8a2ae7b096696787de3533a8a1e34d6c9be743
Sara <so013872@gmail.com>
2025-05-15 14:07:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,29 @@ async function handler(request) {
             });
         }
 
+        if (url.pathname === "/dogbreed") {
+           let breeds = [];
+           let apiUrl = "https://dogapi.dog/api/v2/breeds?page[size]=100";
+           while (apiUrl) {
+               const apiResponse = await fetch(apiUrl);
+               const data = await apiResponse.json();
+               breeds = breeds.concat(
+                   data.data.map(breed => ({
+                       name: breed.attributes.name,
+                       description: breed.attributes.description
+                   }))
+               );
+               apiUrl = data.links && data.links.next ? data.links.next : null;
+           }
+           return new Response(JSON.stringify(breeds), {
+               status: 200,
+               headers: headerCORS
+           });
+       }
+
+
+
+
 
         if (url.pathname === "/dogfact") {
             const apiUrl = "https://dogapi.dog/api/v2/facts";
@@ -43,6 +66,9 @@ async function handler(request) {
         });
 
     }
+
+
+
 }
 
 
